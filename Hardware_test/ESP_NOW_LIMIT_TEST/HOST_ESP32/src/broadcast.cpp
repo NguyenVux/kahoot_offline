e1aef19c9c375bd0b61ad5acba1c6c105c65c295
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-01-16 00:37:37 +0700

    remove redundant code

--- Hardware_test/ESP_NOW_LIMIT_TEST/HOST_ESP32/src/broadcast.cpp
+++ Hardware_test/ESP_NOW_LIMIT_TEST/HOST_ESP32/src/broadcast.cpp
@@ -11,13 +11,8 @@ esp_err_t startBroadcast(const uint8_t *peer_addr,
     size_t counter = 0;
     Stopwatch<> timer;
     esp_err_t err = ESP_OK;
-    // bool (*checkCounter)(size_t counter, size_t num);
-    // checkCounter = (num == 0)
-    //                    ? [](size_t, size_t) { return true; }
-    //                    : [](size_t counter, size_t num) { return counter < num; };
     broadcastLED.blink(100);
     timer.start();
-    broadcastLED.on();
     while (timer.elapsed() <= timeout)
     {
         if (counter < num)
